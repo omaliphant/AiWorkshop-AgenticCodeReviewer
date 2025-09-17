abcdc7f9cf092b3e28ae0d2471a31da9b80af88a
Oz Maliphant <omaliphant@europe.ea.com>
2025-09-17 14:10:12 +0100

    added batch files to run script for demo purposes

---
+++ run_bad_example.bat
@@ -0,0 +1,5 @@
+ECHO "================================ Bad Example ==================================="
+ECHO "Starting AI Code Reviewer with bade code example"
+python ai_code_reviewer.py code_example_bad.py --config agent_config.json --output review_bad.txt
+ECHO "Finished running bad code example"
+ECHO "================================================================================"
---
+++ run_good_example.bat
@@ -0,0 +1,5 @@
+ECHO "================================ Good Example ==================================="
+ECHO "Starting AI Code Reviewer with good code example"
+python ai_code_reviewer.py code_example_good.py --config agent_config.json --output review_good.txt
+ECHO "Finished running good code example"
+ECHO "================================================================================"
